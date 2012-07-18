7bf3b063497f669df325962da657da44bac40907
Alex Gessner <alex.gessner@gmail.com>
2012-07-18 17:26:24 -0400

    testing bitly on prod 8

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,9 +20,8 @@ module ApplicationHelper
 		longlink = longlink + agreement.topic.slug + "/?agreement=" + agreement.id.to_s
 		longlink = "http://www.dailymotion.com/video/xsdji_rick-astley-never-gonna-give-you-up_music"
 		shorturl = Bitly.shorten(longlink, user, apikey) if Rails.env.production?
-		return_this = shorturl.url if Rails.env.production?
-		return_this ||= "http://agrtp.co/MsUNJL"
-		return return_this
+		return shorturl.url if Rails.env.production?
+		return "http://agrtp.co/MsUNJL"
 	end
 
 end
