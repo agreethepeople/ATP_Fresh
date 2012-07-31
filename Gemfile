0a48c703d6412574e86719af0810dfe8725aad9d
Alex Gessner <alex.gessner@gmail.com>
2012-07-31 14:19:54 -0400

    force ssl false instead of comment out

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem "ruby-bitly"
 
 
 gem 'bootstrap-sass', '2.0.0'
-gem 'bcrypt-ruby', '3.0.1'
+#gem 'bcrypt-ruby', '3.0.1'
 gem 'faker', '1.0.1' #for adding fake users
 gem 'will_paginate', '3.0.3'
 gem 'bootstrap-will_paginate', '0.0.6'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ ATPFresh::Application.configure do
 
 ###########################################
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.  
-  # config.force_ssl = true
+   config.force_ssl = false
 ###########################################
 
   # See everything in the log (default is :info)
